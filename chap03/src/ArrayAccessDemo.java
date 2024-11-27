74882859da1f0d77cadf6872a4c4de2bb6452fcc
summer21000 <wkrehreo1999@gmail.com>
2024-11-27 10:52:34 +0900

    2024-11-27 Java 두번째날 실습코드 중간업로드
    Signed-off-by: summer21000 <wkrehreo1999@gmail.com>

---
+++ chap03/src/ArrayAccessDemo.java
@@ -0,0 +1,18 @@
+public class ArrayAccessDemo {
+    public static void main(String[] args) {
+        int[] scores = {100, 90, 50, 95, 85};
+
+        int FirstScore = scores[0];
+        System.out.println("첫 번째 점수 : "+FirstScore);
+
+        int LastScore = scores[scores.length-1];
+        System.out.println("마지막 점수 : "+LastScore);
+
+        System.out.println("배열의 크기 : "+scores.length);
+
+        System.out.println("배열의 모든 원소 : ");
+        for (int i = 0; i< scores.length; i++){
+            System.out.println("원소"+i+" : "+scores[i]);
+        }
+    }
+}
