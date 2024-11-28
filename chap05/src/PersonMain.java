dc8a5719c149ab14280fe7807c78d4d4b6206d35
summer21000 <wkrehreo1999@gmail.com>
2024-11-28 12:18:14 +0900

    2024-11-28 Java 세번째날 실습코드 중간업로드
    Signed-off-by: summer21000 <wkrehreo1999@gmail.com>

---
+++ chap05/src/PersonMain.java
@@ -0,0 +1,9 @@
+public class PersonMain {
+    public static void main(String[] args) {
+        Person person = new Person("Chung Ha");
+        System.out.println("이름 : "+person.getName());
+
+        person.setName("청하");
+        System.out.println("변경된 이름 : "+person.getName());
+    }
+}
