4a09c871c917110eba680bb72876496cd2956397
summer21000 <wkrehreo1999@gmail.com>
2024-11-29 14:13:25 +0900

    2024-11-29 Java 네번째날 실습코드 중간업로드
    Signed-off-by: summer21000 <wkrehreo1999@gmail.com>

---
+++ chap07/src/com/chap7/InterfaceMain.java
@@ -0,0 +1,14 @@
+package com.chap7;
+s
+class MyClass implements MyInterface{
+    public void sayHello(){
+        System.out.println("안녕하세요, 최대값은 : "+MAX);
+    }
+}
+
+public class InterfaceMain {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        myClass.sayHello();
+    }
+}
---
+++ chap07/src/com/chap7/MyInterface.java
@@ -0,0 +1,7 @@
+package com.chap7;
+
+public interface MyInterface{
+    int MAX=10;
+
+    void sayHello();
+}
